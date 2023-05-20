5437c17931116f58efb606711b0a2e5710b26584
HernanGoldy <hernanmdq@gmail.com>
2023-05-20 08:57:59 -0300

    agrego rutas home, location y layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { LocationComponent } from './location/location.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'location',
+        component: LocationComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
